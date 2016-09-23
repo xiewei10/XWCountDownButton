eca36b91ea6c3db7a16a6ba3a877b69d82077d3f
xiewei <978397165@qq.com>
2016-09-23 12:00:03 +0800

    Initial Commit

---
+++ XWCountDownButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWCountDownButton
+//
+//  Created by 谢威 on 16/9/23.
+//  Copyright © 2016年 成都卓牛科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XWCountDownButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XWCountDownButton
+//
+//  Created by 谢威 on 16/9/23.
+//  Copyright © 2016年 成都卓牛科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
